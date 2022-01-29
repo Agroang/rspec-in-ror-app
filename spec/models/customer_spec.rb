c705762cef409b3f09b1630ed862c81347264010
matias acuna <agroang@gmail.com>
2022-01-29 22:05:18 +0900

    added a customer model, the spec and migration, no tests yet

---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+# RSpec.describe is just a being technical, writing describe works fine
+# type: :model this is meta data, is advance RSpec, it allows RSpec to
+# recognize the type of test that it will run
+describe Customer, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
