29322fe175940c47a361fd1ff2625a6fb68094a5
matias acuna <agroang@gmail.com>
2022-01-31 17:56:58 +0900

    added a small test for customer model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_31_064030) do
+ActiveRecord::Schema.define(version: 2022_01_31_085236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2022_01_31_064030) do
   create_table "customers", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
   create_table "orders", force: :cascade do |t|
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,5 +4,9 @@ require 'rails_helper'
 # type: :model this is meta data, is advance RSpec, it allows RSpec to
 # recognize the type of test that it will run
 describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'checks customer attributes writer/reader' do
+    customer = Customer.new
+    customer.name = 'Peter'
+    expect(customer.name).to eq('Peter')
+  end
 end
